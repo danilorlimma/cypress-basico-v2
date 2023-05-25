8e18e60ed0d4d9f2e704d13c656b8cdfdb85fc05
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2023-05-24 22:29:36 -0300

    teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -135,7 +135,6 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('not.have.value')
             .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
             .should(function (input) {
-                //{console.log(input)
                 expect(input[0].files[0].name)
                     .to.equal('example.json')
             });
